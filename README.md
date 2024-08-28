cee15826ba25c138d9ec96270ad9ee6c9b1f922f
yuichi04 <crgone.910428.en@gmail.com>
2024-08-28 09:24:28 +0900

    テストケースを追加

--- README.md
+++
@@ -1,8 +0,0 @@
-# Playwright Course
-This repository is part of the [Playwright - Complete Beginner Course](https://sdetunicorns.com/course/playwright-complete-beginner-course/) on Udemy.
-
-## Install packages
-`npm install`
-
-## Run test
-`npx playwright test {filename}`
--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,63 +1,76 @@
-import { test, expect } from '@playwright/test';
-import HomePage from '../pages/home.page';
+import { test, expect } from "@playwright/test";
 
-test.describe('Home', () => {
-  let homePage: HomePage;
-
-  test.beforeEach(async ({ page }) => {
-    homePage = new HomePage(page);
-    await homePage.navigate()
-  })
-
-  test('Open HomePage and verify title', async ({ page }) => {
-    // verify title
-    await expect(page).toHaveTitle('Practice E-Commerce Site – SDET Unicorns');
-  })
-
-  test.skip('Open About page and verify title', async ({ page }) => {
+test.describe("Home", () => {
+  test("Open HomePage and verify title", async ({ page }) => {
     // open url
-    await page.goto('https://practice.sdetunicorns.com/about');
+    await page.goto("https://practice.sdetunicorns.com/");
 
     // verify title
-    await expect(page).toHaveTitle('About – Practice E-Commerce Site');
-  })
+    await expect(page).toHaveTitle("Practice E-Commerce Site – SDET Unicorns");
+  });
 
-  test('Click get started button using CSS Selector', async ({ page }) => {
-    await expect(page).not.toHaveURL(/.*#get-started/);
+  test("Open About page and verify title", async ({ page }) => {
+    // open url
+    await page.goto("https://practice.sdetunicorns.com/about");
+
+    // verify title
+    await expect(page).toHaveTitle("About – Practice E-Commerce Site");
+  });
+
+  test("Click get started button using CSS Selector", async ({ page }) => {
+    // open url
+    await page.goto("https://practice.sdetunicorns.com/");
 
     // click the button
-    await homePage.getStartedBtn.click()
-    
+    await page.locator("#get-started").click();
+
     // verify url has #get-started
     await expect(page).toHaveURL(/.*#get-started/);
-  })
+  });
 
-  test('Verify heading text is visible using text selector', async ({ page }) => {
+  test("Verify heading text is visible using test selector", async ({
+    page,
+  }) => {
+    // open url
+    await page.goto("https://practice.sdetunicorns.com/");
+
     // find the text locator
-    const headingText = await homePage.headingText
+    const headingText = page.locator(`text="Think different. Make different."`);
 
     // verify heading text is visible
-    await expect(headingText).not.toBeHidden();
     await expect(headingText).toBeVisible();
-  })
+  });
 
-  test('Verify home link is enabled using text and css selector', async ({ page }) => {
+  test("Verify home link is enabled using text and css selector", async ({
+    page,
+  }) => {
+    // open url
+    await page.goto("https://practice.sdetunicorns.com/");
+
     // find the home text
-    const homeText = await homePage.homeLink
+    // const homeText = page.locator('#zak-primary-menu >> text="Home"');
+    const homeText = page.locator('#zak-primary-menu:has-text("Home")');
 
     // verify home text is enabled
     await expect(homeText).toBeEnabled();
-  })
+  });
 
-  test('Verify search icon is visible using xpath selector', async ({ page }) => {
+  test("Verify search icon is visible using epath selector", async ({
+    page,
+  }) => {
+    // open url
+    await page.goto("https://practice.sdetunicorns.com/");
+
     // find the search icon
-    const searchIcon = await homePage.searchIcon
+    const searchIcon = page.locator(
+      '//*[@class="zak-header-actions zak-header-actions--desktop"]//a[@class="zak-header-search__toggle"]'
+    );
 
     // verify search icon is visible
     await expect(searchIcon).toBeVisible();
-  })
+  });
 
-  test('Verify text of all nav links', async ({ page }) => {
+  test("Verify text of all nav links", async ({ page }) => {
     const expectedLinks = [
       "Home",
       "About",
@@ -67,7 +80,13 @@ test.describe('Home', () => {
       "My account",
     ];
 
+    // open url
+    await page.goto("https://practice.sdetunicorns.com/");
+
+    // fine the nav links
+    const navLinks = page.locator("#zak-primary-menu li[id*=menu]");
+
     // verify nav links text
-    expect(await homePage.getNavLinksText()).toEqual(expectedLinks);
-  })
-})
+    expect(await navLinks.allTextContents()).toEqual(expectedLinks);
+  });
+});
