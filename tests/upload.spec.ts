9ccc9aac72e2664ed7bfe4558dfc3507aace4b2b
yuichi04 <crgone.910428.en@gmail.com>
2024-08-31 20:35:11 +0900

    ファイルアップロード処理のテストを作成

--- tests/upload.spec.ts
+++ tests/upload.spec.ts
@@ -7,13 +7,29 @@ test.describe("Upload File", () => {
     await page.goto("https://practice.sdetunicorns.com/cart/");
 
     // provice test file path
-    const filePath = path.join(__dirname, "../data/dummy-150x150.png");
+    const filePath = path.join(__dirname, "../data/dummy.pdf");
+
+    // DOM manipulation
+    await page.evaluate(() => {
+      const selector = document.querySelector("input#upfile_1");
+      if (selector) {
+        selector.className = "";
+      }
+    });
 
     // upload test file
     await page.setInputFiles("input#upfile_1", filePath);
 
     // click the submit button
     await page.locator("#upload_1").click();
+
+    // hardcoded sleep - WRONG WAY
+    // await page.waitForTimeout(5000);
+
+    // wait for condition
+    await page
+      .locator("#wfu_messageblock_header_1_1")
+      .waitFor({ state: "visible", timeout: 10000 });
 
     // assertion
     await expect(page.locator("#wfu_messageblock_header_1_1")).toContainText(
